eba9ac6fa9e6806d561860bb5a2ad50368022585
Tomas Madariaga <critimass@gmail.com>
2016-11-25 20:19:09 -0800

    Routing component generated and app.module configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RoutingComponent } from "./app.routing";
 import { MoviesIndexComponent } from './components/movies-index/movies-index.component';
 import { MovieShowComponent } from './components/movie-show/movie-show.component';
 import { MoviesThemoviedbService } from './services/movies-themoviedb.service'
@@ -18,6 +19,7 @@ import { MoviesThemoviedbService } from './services/movies-themoviedb.service'
     BrowserModule,
     FormsModule,
     HttpModule,
+    RoutingComponent,
     JsonpModule
   ],
   providers: [MoviesThemoviedbService],
---
+++ src/app/app.routing.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+import { MoviesIndexComponent } from './components/movies-index/movies-index.component';
+import { MovieShowComponent } from './components/movie-show/movie-show.component';
+
+const AppRoutes: Routes = [
+  {
+    path: '', component: MoviesIndexComponent
+  },
+  {
+    path: 'movie/:id', component: MovieShowComponent
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(AppRoutes)],
+  exports: [RouterModule],
+  providers: []
+})
+
+export class RoutingComponent {}
