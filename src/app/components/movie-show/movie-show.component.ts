b0213970d8bae908e58c9543c909859e3b14dc3c
Tomas Madariaga <critimass@gmail.com>
2016-11-25 23:20:34 -0800

    movie-show component working

--- src/app/components/movie-show/movie-show.component.ts
+++ src/app/components/movie-show/movie-show.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesThemoviedbService } from '../../services/movies-themoviedb.service'
+import {Router, ActivatedRoute} from '@angular/router'
 
 @Component({
-  selector: 'app-movie-show',
+  selector: 'movie',
   templateUrl: './movie-show.component.html',
   styleUrls: ['./movie-show.component.scss']
 })
 export class MovieShowComponent implements OnInit {
+  movie: any = {};
+  similarMovies: Array<Object>;
 
-  constructor() { }
+  constructor(private moviesService: MoviesThemoviedbService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.params
+      .map(params => params['id'])
+      .subscribe( id =>{
+        console.log(id)
+        this.moviesService.getMovieDetails(id)
+          .subscribe(result => {
+            console.log(result)
+            this.movie = result
+          })
+      })
+
+      this.activatedRoute.params
+      .map(params => params['id'])
+      .subscribe( id =>{
+        console.log(id)
+        this.moviesService.getSimilarMovies(id)
+          .subscribe(response => {
+            console.log(response)
+            this.similarMovies = response.results
+          })
+      })
   }
 
 }
